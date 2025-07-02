d7be460bcc22cedcf3dc16527b43e6759d4e82f2
paulo.reis <paulo.reis@alunos.iuul.com.br>
2025-07-02 14:34:05 -0400

    Tarefa prática #2 - Serviço de Avaliações

--- services/controller/index.js
+++ services/controller/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const shipping = require('./shipping');
 const inventory = require('./inventory');
+const reviews = require('./review'); // Importa o cliente do serviço de avaliações
 const cors = require('cors');
 
 const app = express();
@@ -65,4 +66,35 @@ app.get('/product/:id', (req, res, next) => {
  */
 app.listen(3000, () => {
     console.log('Controller Service running on http://127.0.0.1:3000');
+});
+
+// Rota para obter avaliações de um produto
+app.get('/reviews/:id', (req, res, next) => {
+    reviews.GetReviews({ id: req.params.id }, (err, reviewsData) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ error: 'Falha ao recuperar avaliações' });
+        } else {
+            res.json(reviewsData);
+        }
+    });
+});
+
+// Rota para adicionar uma nova avaliação
+app.post('/reviews', (req, res, next) => {
+    const review = {
+        productId: req.body.productId,
+        username: req.body.username,
+        rating: req.body.rating,
+        comment: req.body.comment,
+    };
+
+    reviews.AddReview(review, (err, response) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ error: 'Falha ao adicionar avaliação' });
+        } else {
+            res.json(response);
+        }
+    });
 });
